fcec34d917500f0659f4ebb1a66046a811f4663a
younes talibi <younessetalibi11@gmail.com>
2023-05-24 18:25:26 +0100

    one of the biggest modifications i ever did in my entire day

--- ai_pedia_react/src/pages/AIPedia/Home.jsx
+++ ai_pedia_react/src/pages/AIPedia/Home.jsx
@@ -30,61 +30,61 @@ function Home(){
 
     return (
         
-        <>
-        {data?
-        <div className='pedia-container'>
-        <div className='pedia-header'>
-            <div className='pedia-title'>
-                AI Pedia -Ready to take your productivity to the next level?
-            </div>
-            <div className='pedia-subtitle'>
-                Find & Save the best AI tools every day for 10X results!
-            </div>
-            <div className='pedia-explore-button'>
-                <button >
-                    EXPLORE ALL CATEGORIES
-                </button>
-            </div>
-        </div>
-        <div className='pedia-search'>
-            <input type="text" placeholder='I Am Looking For ...'
-            onChange={(e) => setSearch(e.target.value)} 
-            />
-            <div onClick={()=>setFilter(search)}>
-                {/* <AiOutlineSearch/> */}
-                <Link to={`/search/${search}`}>search</Link>
-            </div>
-        </div>
-        <div className='pedia-catigories'>
-            {data.AIPediaToolsCategories.map((e,i)=>(
-                <div key={i}>
-                    {e.name}
+        <div className='container'> 
+            {data?
+            <div className='pedia-container'>
+            <div className='pedia-header'>
+                <div className='pedia-title'>
+                    AI Pedia -Ready to take your productivity to the next level?
                 </div>
-            ))}
-        </div>
-        <div className='pedia-items-title'>
-            🔥 Just Arrives
-        </div>
-        <AIPediaCards search={filter}/>
-        <div className='pedia-discord'>  
-            <div>
-                <h1>Grow your AI talent !</h1>
-                <div className='pedia-discord-first'>Join our Discord community and win a free subscription of the most popular AI tool!</div>
-                <div className='pedia-discord-second'>Level up your skills with daily games and competitions</div>
+                <div className='pedia-subtitle'>
+                    Find & Save the best AI tools every day for 10X results!
+                </div>
+                <div className='pedia-explore-button'>
+                    <button >
+                        EXPLORE ALL CATEGORIES
+                    </button>
+                </div>
             </div>
-            <div className='pedia-discord-button'>
-                JOIN DISCORD 👌
+            <div className='pedia-search'>
+                <input type="text" placeholder='I Am Looking For ...'
+                onChange={(e) => setSearch(e.target.value)} 
+                />
+                <div onClick={()=>setFilter(search)}>
+                    {/* <AiOutlineSearch/> */}
+                    <Link to={`/search/${search}`}><i class="bi bi-search"></i></Link>
+                </div>
             </div>
+            <div className='pedia-catigories'>
+                {data.AIPediaToolsCategories.map((e,i)=>(
+                    <div key={i}>
+                        {e.name}
+                    </div>
+                ))}
+            </div>
+            <div className='pedia-items-title'>
+                🔥 Just Arrives
+            </div>
+            <AIPediaCards search={filter}/>
+            <div className='pedia-discord'>  
+                <div>
+                    <h1>Grow your AI talent !</h1>
+                    <div className='pedia-discord-first'>Join our Discord community and win a free subscription of the most popular AI tool!</div>
+                    <div className='pedia-discord-second'>Level up your skills with daily games and competitions</div>
+                </div>
+                <div className='pedia-discord-button'>
+                    JOIN DISCORD 👌
+                </div>
+            </div>
+            <div className='pedia-items-title'>
+                🙌 Manifest AI selection
+            </div>
+            <AIPediaCards search={filter}/>
+            </div> 
+            :
+            <Loader/>
+            }
         </div>
-        <div className='pedia-items-title'>
-            🙌 Manifest AI selection
-        </div>
-        <AIPediaCards search={filter}/>
-        </div> 
-        :
-        <Loader/>
-        }
-        </>
     );
 }
 
