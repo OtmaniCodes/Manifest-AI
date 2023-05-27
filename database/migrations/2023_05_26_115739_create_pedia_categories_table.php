3b6882c7a784908cac734def01aaf4fb2b41e2a4
younes talibi <younessetalibi11@gmail.com>
2023-05-27 19:48:35 +0100

    updates

--- database/migrations/2023_05_26_115739_create_pedia_categories_table.php
+++ database/migrations/2023_05_26_115739_create_pedia_categories_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('pedia_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
